2e34d3d14c9583b4ee7e5b51fb23122586ba5ae2
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2021-05-03 19:08:27 +0200

    Clarified code to compute distance distributions

--- code/simulation/hist_distrib.r
+++ code/simulation/hist_distrib.r
@@ -24,10 +24,10 @@ log10Tck <- function(side, type){
 
 
 #Theoretical value of G
-G_theoretical=function(gamma,rDelta,C_0,U="1",tmax=NA){ #U=0 in the absence of advection
+G_theoretical=function(gamma,rDelta,C_0,tmax=NA){ #U=0 in the absence of advection
         tau=1
         lambda=0.5
-        if(U!="0"){
+        if(gamma>0){
         tmp=-1*lambda*tau/(2*pi*C_0*Delta^2)*(log(gamma)-log(1/(tau*rDelta^2)+gamma)) #Assuming that the advection U > 0
         }else{ #This corresponds to the case U=0
         D=Delta^2/(2*tau)
@@ -37,30 +37,54 @@ G_theoretical=function(gamma,rDelta,C_0,U="1",tmax=NA){ #U=0 in the absence of a
 }
 
 
-par(mfrow=c(1,2))
-f=read.table('pcf_dpow0p25_tmax1000_U0p1_test.txt',sep=";",header=F,dec=".")
-colnames(f)=c("rDelta","U","pcf")
+pdf("pcf_and_distribution_pairdist_1000part.pdf",height=4,width=10)
+par(mfrow=c(1,3))
 
-f=unique(f)
+U_unique=c("tmax1000_U0p0","tmax5000_U0p0","tmax1000_U2p5")
+val_U=c(0,0,2.5)
+tmax=c(1000,5000,1000)
 
-plot(0.1,0.1,t="n",xlim=range(Delta*f$rDelta),ylim=c(0.01,max(f$pcf)),log="xy",xlab="r",ylab="g(r,t)-1",axes=F,main="Utau/2=0.1")
+U_unique=c("tmax2000_U0p0")
+val_U=c(0)
+tmax=c(2000)
 
-axis(1, at=log10Tck('x','major'), tcl= 0.2) # bottom
-axis(2, at=log10Tck('y','major'), tcl= 0.2) # left
-axis(1, at=log10Tck('x','minor'), tcl= 0.1, labels=NA) # bottom
-axis(2, at=log10Tck('y','minor'), tcl= 0.1, labels=NA) # left
-box()
 
-f0=subset(f,U==0.1)
-lines(Delta*f0$rDelta,f0$pcf-1,lty=1,col="black",lwd=2,t="o")
-lines(Delta*f0$rDelta,G_theoretical(0.026,f0$rDelta,19450/area,"1",1000),col="black",lty=2,lwd=2)
+for (u in 1:length(U_unique)){
 
-tt=read.table("nb_individuals_dpow0p25_intervalle_tmax1000_U0p1_test.txt",sep=";",header=F,skip=1)
-colnames(tt)=c("Utot","pow","nb")
+	f=read.table(paste('pcf_dpow0p25_',U_unique[u],'_table_distance.txt',sep=""),sep=";",header=F,dec=".")
+	colnames(f)=c("rDelta","U","pcf")
 
-U_unique=unique(tt$Utot)
+	tt=read.table(paste("nb_individuals_dpow0p25_intervalle_",U_unique[u],"_table_distance.txt",sep=""),sep=";",header=F,nrows=1)
+	nb_ind=tt[1,2]
+	C_0=nb_ind/area
 
-for(U in 1:length(U_unique)){
-	plot(-1*tt$pow,tt$nb/2,log="y",main=paste("Utau/2=",U_unique[U]),xlab="dist ij",ylab="Nb",xaxis="n")
+	plot(0.1,0.1,t="n",xlim=range(f$rDelta),ylim=c(0.01,max(f$pcf)),log="xy",xlab="r",ylab="g(r,t)-1",axes=F,main=paste("Utau/2=",val_U[u],", tmax=",tmax[u],sep=""),cex.axis=1.5,cex.lab=1.5,cex.main=1.5)
+	axis(1, at=log10Tck('x','major'), tcl= 0.2,cex.axis=1.5) # bottom
+	axis(2, at=log10Tck('y','major'), tcl= 0.2,cex.axis=1.5) # left
+	axis(1, at=log10Tck('x','minor'), tcl= 0.1, labels=NA) # bottom
+	axis(2, at=log10Tck('y','minor'), tcl= 0.1, labels=NA) # left
+	#plot(0.1,0.1,t="n",xlim=range(log10(f$rDelta)),ylim=c(0.01,max(f$pcf)),log="y",xlab="log10(r)",ylab="g(r,t)-1",axes=T,main=paste("Utau/2=",val_U[u],", tmax=",tmax[u],sep=""),cex.axis=1.5,cex.lab=1.5,cex.main=1.5)
+
+	box()
+
+	#lines(log10(Delta*f$rDelta),f$pcf-1,lty=1,col="black",lwd=2,t="o")
+	#lines(log10(Delta*f$rDelta),G_theoretical(val_U[u],f$rDelta,C_0,tmax[u]),col="black",lty=2,lwd=2)
+	lines(f$rDelta,f$pcf-1,lty=1,col="black",lwd=2,t="o")
+	lines(f$rDelta,G_theoretical(val_U[u],f$rDelta/Delta,C_0,tmax[u]),col="black",lty=2,lwd=2)
+
+	tt=read.table(paste("nb_individuals_dpow0p25_intervalle_",U_unique[u],"_table_distance.txt",sep=""),sep=";",header=F,skip=1)
+	colnames(tt)=c("Utot","pow","nb")
+
+	plot(-1*tt$pow,tt$nb/2,log="y",xlab="dist ij",ylab="Nb pairs",xaxt="n",t="h",lwd=2,cex.axis=1.5,cex.lab=1.5,cex.main=1.5)
+	axis(1,label=c("<=10^(-10)",paste(10^(seq(-8,0,by=2)))),at=seq(-10,0,by=2),cex.axis=1.5,cex.lab=1.5)
 	#xlim=c(10^(-10),0.01)
+
+tt=read.table("distance_table.txt",sep=";",header=T)
+colnames(tt)=c("i","j","dist")
+tt=unique(tt)
+
+hist(log10(tt$dist))
+axis(1,seq(-15,0,by=1))
 }
+
+dev.off()
--- code/simulation/main_Fig3.cpp
+++ code/simulation/main_Fig3.cpp
@@ -21,8 +21,8 @@ extern const double Delta=pow(10,-7); //diffusion
 extern const double Lmax=pow(10,0.5); //size of the grid
 extern const double area=Lmax*Lmax; //size of the grid
 extern const double k=2*pi;// We do not divide by Lmax, i.e. we do not change the wavenumber even though Lmax changes 
-extern const int size_pop=50000; //initial number of particles
-extern const int tmax=1000; //length of the simulation
+extern const int size_pop=500; //initial number of particles
+extern const int tmax=2000; //length of the simulation
 extern const double proba_death=0.5; //Death and birth probability
 extern const double proba_repro=0.5;
 
@@ -41,14 +41,13 @@ double distance(basic_particle p1, basic_particle p2)
 }
 
 //Compute the pair density. WARNING: this function may not be completely accurate, as it may sligthly underestimate the pair density
-double PairDens(double xi, double dxi, std::vector<basic_particle> Part_table, int repart[])
+double PairDens(double xi, double dxi, std::vector<basic_particle> Part_table)
 {
 double d2,dt2,dtt2;
 int ki,kj;
 basic_particle temp, current;
 int iter=0;
 int p1=0,p2=0;
-int pow_dist,id_pow;
     
     for(p1=0;p1<Part_table.size();p1++)
 {   
@@ -69,11 +68,8 @@ int pow_dist,id_pow;
                         dtt2 = d2;
                       }
              }
-		pow_dist=int(std::max(int(-10),int(round(log10(d2)))));
-//		std::cout<<"pow dist "<<pow_dist<<std::endl;
-		id_pow=-1*pow_dist;
-//		std::cout<<"id pow "<<id_pow<<std::endl;
-		repart[id_pow]=repart[id_pow]+1;
+
+		}	
                 
                 // squared distance computed
                 // if distance between xi and xi+dxi increment PairDens
@@ -83,8 +79,53 @@ int pow_dist,id_pow;
                 }
               }
         }
-}
         return iter/(pi*(pow(xi+dxi,2.0) - pow(xi,2.0))*area); // number of pairs between xi and xi+dxi divided by the area of the crown
+}
+
+void distrib_distance(std::vector<basic_particle> Part_table, int repart[])
+{
+double d2,dt2,dtt2;
+int ki,kj;
+basic_particle temp, current;
+int iter=0;
+int p1=0,p2=0;
+int pow_dist,id_pow;
+std::ofstream f3;
+
+
+f3.open("distance_table.txt");
+
+    for(p1=0;p1<Part_table.size();p1++)
+{
+    current=Part_table.at(p1);
+    // double loop (or loop on all pairs)
+    for (p2=0;p2<Part_table.size();p2++)
+{
+        if(p1!=p2)
+{
+                temp=Part_table.at(p2);
+                dtt2=area;
+                for (ki=-1;ki<=1;ki++)
+            {
+                    for (kj=-1;kj<=1;kj++)
+                    {
+                        dt2 = pow((temp.get_x() - current.get_x()+ki*Lmax),2) + pow((temp.get_y() - current.get_y()+kj*Lmax),2);
+                        d2 = std::min(dtt2,dt2);
+                        dtt2 = d2;
+                      }
+             }
+
+                 if(p2<p1){
+                 	pow_dist=int(std::max(int(-10),int(round(log10(pow(d2,0.5))))));
+                        id_pow=-1*pow_dist;
+                        repart[id_pow]=repart[id_pow]+1;
+                        f3<<p1<<";"<<p2<<";"<<pow(d2,0.5)<<std::endl;
+                        }
+
+}
+}
+}
+f3.close();
 }
 
 //Births and deaths in a community. The current vector of particles is updated with the new particles (at the end of the table), then we remove the dead ones
@@ -119,18 +160,22 @@ int main()
 	double a_x,a_y,phi,theta,a_n,xi,dxi,pow_min,pow_max,dpow,pow_i,pcf,C;
 	std::vector<basic_particle> Part_table,Part_table_tmp;
 	//std::vector<double> Utot_list{ 0.0, 0.1, 0.5,2.5 }; //The structure of the code enables lauching one simulation for all Utot. For speed purposes, though, it is preferable to launch one simulation per Utot
-	std::vector<double> Utot_list{2.5};
+	std::vector<double> Utot_list{0.0};
 	std::ofstream f0,f1;
 
 	dxi=pow(10,-8);
-	pow_min=-1+log10(Delta);pow_max=5.5+log10(Delta); //These are the limits in Fig. 3 of Young et al. 2001
-	//pow_min=-5;pow_max=-4.6; //These are the limits in Fig. 3 of Young et al. 2001
-	dpow=0.25;
+	//pow_min=-1+log10(Delta);pow_max=5.5+log10(Delta); //These are the limits in Fig. 3 of Young et al. 2001
+	//dpow=0.25;
+	pow_min=-10;pow_max=0; //These are the limits in Fig. 3 of Young et al. 2001
+	dpow=1.0;
 	int repart[11];
+	for(i=0;i<11;i++){
+		repart[i]=0;
+	}
 
 	//Open the file in which we will have the x, y, parent of each particle
-	f0.open("nb_individuals_dpow0p25_intervalle_tmax1000_U2p5_test.txt");
-	f1.open("pcf_dpow0p25_tmax1000_U2p5_test.txt");
+	f0.open("nb_individuals_dpow0p25_intervalle_tmax2000_U0p0_table_distance.txt");
+	f1.open("pcf_dpow0p25_tmax2000_U0p0_table_distance.txt");
 
 	for (double Utot : Utot_list) 
 	{
@@ -168,9 +213,6 @@ int main()
 	while (pow_i<pow_max)
 //	while (xi<xi_max)
 	{
-	for(i=0;i<11;i++){
-		repart[i]=0;
-	}
 	
 		xi=pow(10,pow_i);
 //		dxi=min(pow(10,pow_i),0.001);
@@ -178,23 +220,19 @@ int main()
 		std::cout<<"xi "<<xi<<std::endl;
 		std::cout<<"xi-dxi "<<xi-dxi<<std::endl;
 		std::cout<<"xi+dxi "<<xi+dxi<<std::endl;
-		pcf=PairDens(xi,dxi,Part_table,repart)/(pow(C,2));
-		f1<<xi/Delta<<";";
+		pcf=PairDens(xi,dxi,Part_table)/(pow(C,2));
+		f1<<xi<<";";
 		f1<<Utot<<";";
 		f1<<pcf<<std::endl;
 		pow_i=pow_i+dpow;
 	}
 	f0<<Utot<<";"<<Part_table.size()<<std::endl;
+	distrib_distance(Part_table,repart);
 	 Part_table= std::vector<basic_particle>(); //Deallocate, reinitialize
-
 	for(i=0;i<11;i++){
 		f0<<Utot<<";"<<i<<";"<<repart[i]<<std::endl;
 	}
 
-	for(i=0;i<11;i++){
-		repart[i]=0;
-	}
-	
 	} //End loop on Utot
 
 	
